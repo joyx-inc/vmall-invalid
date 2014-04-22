1de8c0befc8355cee981142ff40635a61d91ff68
poorevil <poorevil@gmail.com>
2014-04-22 22:38:12 +0800

    多了一个api目录

--- api/urls.py
+++ api/urls.py
@@ -4,6 +4,6 @@ from api import views
 urlpatterns = patterns('api.views',
                        # Examples:
                        # url(r'^$', 'vmall.views.home', name='home'),
-                       url(r'^api/list_store$', views.list_store, name='list_store'),
+                       url(r'^list_store$', views.list_store, name='list_store'),
 
 )
