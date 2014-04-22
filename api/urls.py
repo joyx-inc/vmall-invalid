1b3e4a82304a96283672ad8164cbc3708c4b82a1
poorevil <poorevil@gmail.com>
2014-04-22 22:42:02 +0800

    添加注释

--- api/urls.py
+++ api/urls.py
@@ -4,6 +4,6 @@ from api import views
 urlpatterns = patterns('api.views',
                        # Examples:
                        # url(r'^$', 'vmall.views.home', name='home'),
-                       url(r'^list_store$', views.list_store, name='list_store'),
+                       url(r'^list_store$', views.list_store, name='list_store'),# 关于url()的name用法：https://docs.djangoproject.com/en/1.6/topics/http/urls/#naming-url-patterns
 
 )
